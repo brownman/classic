5d5498f5ce2acd59631bca091ff94a9e1ee8399a
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-04 20:52:08 -0500

    Fixed typo.

--- tmp/scripts/travis_install.sh
+++ tmp/scripts/travis_install.sh
@@ -17,6 +17,7 @@ cd ../../../
 cd ../
 mv classic profile
 drush make --prepare-install profile/build-classic.make drupal --yes
+cd drupal
 sudo chmod -R 777 sites/all/modules
 drush si classic --sites-subdir=default --db-url=mysql://root:@127.0.0.1/drupal --account-name=admin --account-pass=classic --site-mail=admin@example.com --site-name="Drupal Classic Profile" --yes
 drush cc all --yes
