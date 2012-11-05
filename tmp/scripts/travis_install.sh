a03eac960cd2515a2477aaeee1729edb2d7f2a44
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-04 21:14:05 -0500

    Added WORKSPACE variable.

--- tmp/scripts/travis_install.sh
+++ tmp/scripts/travis_install.sh
@@ -2,18 +2,17 @@
 
 # Install Classic Profile
 cd ../
+export WORKSPACE=$PWD
+echo "WORKSPACE set to: $WORKSPACE"
 mv classic profile
 drush make --prepare-install profile/build-classic.make drupal --yes
 cd drupal
-sudo chmod -R 777 sites/all/modules
 drush si classic --sites-subdir=default --db-url=mysql://root:@127.0.0.1/drupal --account-name=admin --account-pass=classic --site-mail=admin@example.com --site-name="Drupal Classic Profile" --yes
 drush cc all --yes
-cd ../
 
 # Run composer
-cd drupal/profiles/classic/tmp/tests/behat
+cd $WORKSPACE/drupal/profiles/classic/tmp/tests/behat
 composer install
-cd ../../../../../..
 
 # Create drush alias
-cp drupal/profiles/classic/tmp/tests/behat/classic.aliases.drushrc.php ~/.drush/
+cp $WORKSPACE/drupal/profiles/classic/tmp/tests/behat/classic.aliases.drushrc.php ~/.drush/
