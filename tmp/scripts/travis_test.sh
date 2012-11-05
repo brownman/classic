73eb7d624f8a3a1058829af1d9de37548e8883ed
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-05 00:01:01 -0500

    Bumped selenium server warm-up time.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -6,7 +6,7 @@ sleep 3 # give xvfb some time to rebuild
 
 wget http://selenium.googlecode.com/files/selenium-server-standalone-$SELENIUM_VERSION.jar
 java -jar selenium-server-standalone-$SELENIUM_VERSION.jar -p 4444 &
-sleep 5
+sleep 10
 cd $WORKSPACE/build/profiles/$PROJECT_NAME/tmp/tests/behat
 touch behat.local.yml
 ./bin/behat
