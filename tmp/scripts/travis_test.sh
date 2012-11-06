c43b7d04e8527c7080e93e5aab0faa018af4d2d9
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-06 12:21:55 -0500

    Test each suite for failures but run through all.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# Fail when any test suite fails.
-set -e
-
-sleep 5
+STATUS=0
 
 # Run CasperJS
 cd $WORKSPACE
 DISPLAY=:99.0 ./casperjs/bin/casperjs test $WORKSPACE/build/profiles/$PROJECT_NAME/tmp/tests/casperjs/
+[ $? -eq 0 ] || STATUS=1
 
 # Run selenium server for Behat
 DISPLAY=:99.0 java -jar $WORKSPACE/selenium-server.jar -p 4444 &
@@ -17,3 +15,7 @@ sleep 10
 cd $WORKSPACE/build/profiles/$PROJECT_NAME/tmp/tests/behat
 touch behat.local.yml
 ./bin/behat
+[ $? -eq 0 ] || STATUS=1
+
+# Exit with error if any of the suites fail (non-zero exits on suites set STATUS=1)
+exit $STATUS
